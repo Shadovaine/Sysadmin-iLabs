496da6a9ab33c70d32b7728654e2f4176f506b76
Shadovaine <shado.sec@proton.me>
2026-09-13 19:49:01 -0500

    Moved analyse_log.sh and auth.log under log_analysis

--- health-checks/health-checks.sh
+++ health-checks/health-checks.sh
@@ -7,27 +7,73 @@ echo "=====System Health Report====="
 echo "Generated: $(date)"
 echo
 
-echo "===== Uptime ====="
+echo
+echo "========================"
+echo " Uptime"
+echo "========================"
 uptime
 echo
 
-echo "===== Memory Usage ====="
+echo
+echo "========================"
+echo " Memory Usage"
+echo "========================"
 free -h
 echo
 
-echo "===== Dish Usage ====="
+echo
+echo "========================"
+echo " Disk Usage"
+echo "========================"
 df -h
 echo
 
-echo "===== Top 5 Memory Consuming Processes ====="
+echo
+echo "========================="
+echo " Health Warnings"
+echo "========================="
+
+MEMORY_USED=$(free | awk '/Mem:/ {printf("%.0f", $3/$2 * 100)}')
+
+if [ "$MEMORY_USED" -ge 80 ]; then
+    echo "WARNING: Memory Usage is ($MEMORY_USED)%"
+else
+    echo "OK: Memory Usage is ($MEMORY_USED)%"
+fi
+
+echo
+
+df -h
+do
+    USAGE_NUM=$(echo "$usage" | tr -d '%')
+
+    if [ "$USAGE_NUM" -ge 80 ]; then
+	echo "WARNING: $mount is ($USAGE_NUM}% full"
+    else
+	echo "OK: $mount is ($USAGE_NUM)% full"
+    fi
+done
+
+echo
+
+echo
+echo "======================================="
+echo " Top 5 Memory Consuming Process"
+echo "======================================="
+echo
 ps aux --sort=-%mem | head -6
 echo
 
-echo "===== Top 5 CPU Consuming Process ====="
+echo
+echo "======================================="
+echo " Top 5 CPU Consuming Process"
+echo "======================================="
+echo
 ps aux --sort=-%cpu | head -6
 echo
 
 } | tee "$REPORT"
+
 
 echo
 echo "Report saved to $REPORT"
